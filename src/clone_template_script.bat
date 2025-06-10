1456691d5c97256d47a86922376741d100da1d52
HardCodeDev <hardcodedev777@yandex.com>
2025-06-10 16:04:25 +0400

    Add first files

---
+++ src/clone_template_script.bat
@@ -0,0 +1,19 @@
+@echo off
+chcp 65001 > nul
+
+set /p github_username="Your GitHub username: "
+set /p repo_name="Repository name: "
+set repo_url=https://github.com/%github_username%/%repo_name%.git
+
+echo Cloning repository: %repo_url%
+git clone %repo_url%
+echo Repository cloned successfully!
+
+copy "LICENSE.txt" "%repo_name%\LICENSE.txt" > nul
+echo LICENSE.txt copied!
+
+cd %repo_name% > nul
+md src > nul
+echo Folder src created successfully!
+
+pause
